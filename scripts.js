2ca9e3febff90287df00a81dcf63ed78cd9c64bc
macca1b <brendanmaccabe@gmail.com>
2024-07-05 13:25:09 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
